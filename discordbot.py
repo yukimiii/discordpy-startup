502bc74b32232bab99689cba9c27841bac1b4eb1
yukimiii <70323772+yukimiii@users.noreply.github.com>
2021-03-04 01:02:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,13 +8,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
 @client.event
 async def on_reaction_add(reaction,user):
 	if reaction.emoji=="📧":
@@ -29,4 +22,4 @@ async def on_reaction_add(reaction,user):
 		await user.send(embed=embed)
 		# await user.send(reaction.message.content)
 
-bot.run(token)
+client.run(TOKEN)
