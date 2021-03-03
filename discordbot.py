b945dca13c90036a730e5f232042df6482090ea0
yukimiii <70323772+yukimiii@users.noreply.github.com>
2021-03-04 00:51:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,18 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+@client.event
+async def on_reaction_add(reaction,user):
+	if reaction.emoji=="📧":
+		embed = discord.Embed(description=reaction.message.content)
+		embed.set_author(name=reaction.message.author.display_name,icon_url=reaction.message.author.avatar_url)
+		# ファイルがある場合
+		if reaction.message.attachments:
+			for attachment in reaction.message.attachments:
+				# Attachmentの拡張子がpng, jpg, jpegのどれかだった場合
+				if attachment.url.endswith(("png", "jpg", "jpeg","JPG")):
+					embed.set_image(url=attachment.url)
+		await user.send(embed=embed)
+		# await user.send(reaction.message.content)
 
 bot.run(token)
